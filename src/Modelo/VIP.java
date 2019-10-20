018dc8e4b93332b2dab87cbda97db301052c3ec2
Dereck <dereckescalante@hotmail.com>
2019-10-20 10:50:49 -0600

    ventanas

--- src/Modelo/VIP.java
+++ src/Modelo/VIP.java
@@ -3,8 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package redsocial;
+package Modelo;
 
+import Modelo.User;
+import Modelo.Follower;
 import java.util.ArrayList;
 
 /**
